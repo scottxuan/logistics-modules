7586e0ec515532808ea3db408f49e9177c7a3e07
zhaoxuan <zhaoxuan@jueeyaoair.com>
2020-07-21 17:29:29 +0800

    INIT

--- module-common/src/main/java/com/module/common/constants/CacheConstant.java
+++ module-common/src/main/java/com/module/common/constants/CacheConstant.java
@@ -4,5 +4,7 @@ package com.module.common.constants;
  * @author : scottxuan
  */
 public interface CacheConstant {
-
+    String PREFIX_MENU = "MENU:";
+    String PREFIX_PERMISSION = "PERMISSION:";
+    String PREFIX_SMS_CODE = "SMS_CODE:";
 }
