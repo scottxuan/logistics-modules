0eb8b4d644fc70b4f361cd1ff6ed4154cdb85d6c
scottxuan <zhaoxuan734@163.com>
2020-08-23 21:02:55 +0800

    INIT

--- module-system/src/main/java/com/module/system/dto/SysConfigDto.java
+++ module-system/src/main/java/com/module/system/dto/SysConfigDto.java
@@ -1,7 +1,7 @@
 package com.module.system.dto;
 
 import com.module.system.entity.SysConfig;
-import com.scottxuan.base.pair.CodeNamePair;
+import com.scottxuan.base.pair.Pair;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -15,7 +15,7 @@ public class SysConfigDto extends SysConfig {
     private static final long serialVersionUID = 1664263438860893571L;
 
     @ApiModelProperty(value = "值列表")
-    private List<CodeNamePair> valueList;
+    private List<Pair<String,String>> valueList;
 
     @ApiModelProperty(value = "值")
     private List<String> selectValueList;
